d2fd52531222cf3655cc3ec191a0a89b1847b85f
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-03-03 15:27:01 +0100

    add Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,12 @@
 import './App.css'
-import styled from "styled-components";
+import {Header} from "./site/Header.tsx";
+
 
 function App() {
     return (
-        <div className="App">
-            <Title>Welcome to IT-INCUBATOR</Title>
-        </div>
+        <Header />
     )
 }
 
 export default App
 
-const Title = styled.h1`
-    font-size: 1.5em;
-    text-align: center;
-    color: #e91e63;
-`;
