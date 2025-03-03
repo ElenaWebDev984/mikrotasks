f5bb91029f65df8d3dc519fc2d51fb1e34bdc5fc
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-03-03 16:52:11 +0100

    add map for Array<students>

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
 import './App.css'
-import {Header} from "./site/Header.tsx";
-import {Body} from "./site/Body.tsx";
-import {Footer} from "./site/Footer.tsx";
+import {NewComponent, students} from "./site/NewComponent.tsx";
+
+
 
 
 function App() {
     return (
         <>
-            <Header title={'NEW HEADER'}/>
-            <Body title={'NEW BODY'}/>
-            <Footer title={'NEW FOOTER'}/>
+          <NewComponent students={students} />
         </>
     )
 }
