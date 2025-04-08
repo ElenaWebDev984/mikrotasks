f81217229650956b31cdeebc53c2ab719daf1c35
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-04-09 00:09:43 +0200

    training map() with ul and table

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import './App.css'
 import {NewComponent, students} from "./site/NewComponent.tsx";
 import {NewComponentTopCars, topCars} from "./site/NewComponentTopCars.tsx";
+import {cities, Cities} from "./site/Cities.tsx";
+import {people, People} from "./site/People.tsx";
+
 
 
 function App() {
@@ -8,6 +11,8 @@ function App() {
         <>
             <NewComponent students={students}/>
             <NewComponentTopCars topCars={topCars} />
+            <Cities cities={cities} />
+            <People people={people} />
         </>
     )
 }
