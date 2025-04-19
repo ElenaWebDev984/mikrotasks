d732ef71fd47bccc16fe6f772200724086aa1ddf
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-04-19 17:15:04 +0200

    useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,15 @@
 import './App.css'
-import {Button} from "./components/Button.tsx";
+
 
 
 
 function App() {
-    const Button1Foo = (subscriber: string, age: number, address: string) => {
-        console.log(subscriber, age, address);
-    }
-
-    const Button2Foo = (subscriber: string) => {
-        console.log(subscriber);
-    }
-
-    const Button3Foo = () => {
-        console.log("I'm stupid button");
-    }
+    const a = 1
 
     return (
-        <>
-            <Button name={'MyYouTubeChanel-1'} callBack={() => Button1Foo("I'm Vasya", 21, 'Minsk')}/>
-            <Button name={'MyYouTubeChanel-2'} callBack={() =>  Button2Foo("I'm Ivan")}/>
-            <Button name={"Stupid button"} callBack={Button3Foo}/>
-        </>
+        <div className="App">
+            <h1>{a}</h1>
+        </div>
     )
 }
 
