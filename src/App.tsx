faeff964d44ccc4a38fd5bef16810cc44bb7d22b
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-04-19 17:19:06 +0200

    added counter for button

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,17 @@ import './App.css'
 
 
 function App() {
-    const a = 1
+    let a = 1
+
+    const onClickHandler = () => {
+        ++a;
+        console.log(a)
+    }
 
     return (
         <div className="App">
             <h1>{a}</h1>
+            <button onClick={onClickHandler}>number</button>
         </div>
     )
 }
