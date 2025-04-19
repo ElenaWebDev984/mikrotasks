5694346d1eae04593e54eb7fad39fb2a01487b10
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-04-19 17:31:55 +0200

    completed tsk useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,29 @@
 import './App.css'
+import {useState} from "react";
 
 
 
 
 function App() {
-    let a = 1
+    // let a = 1
+    let [a, setA] = useState(1)
+    // TODO a - это состояние (initial state/ local state), может быть чем угодно: [], {} и т.д.
+    //  setA - это функция в которой находится состояние
 
     const onClickHandler = () => {
-        ++a;
+        setA(++a);
         console.log(a)
+    }
+
+    const onClickHandler2 = () => {
+        setA(0);
     }
 
     return (
         <div className="App">
             <h1>{a}</h1>
             <button onClick={onClickHandler}>number</button>
+            <button onClick={onClickHandler2}>0</button>
         </div>
     )
 }
