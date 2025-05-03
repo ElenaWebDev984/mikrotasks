902c30d11449a3b04008a651ed7a66f6d64ed531
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-05-03 17:18:59 +0200

    init task input

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,24 @@
 import './App.css'
 import {useState} from "react";
-import {NewComponentForFilterTask} from "./NewComponentForFilterTask.tsx";
 
-export type FilterType = 'all' | 'ruble' | 'dollar';
 
 
 function App() {
-    const [money, setMoney] = useState([
-        { banknote: 'Dollars', nominal: 100, number: ' a1234567890' },
-        { banknote: 'Dollars', nominal: 50, number: ' z1234567890' },
-        { banknote: 'RUBLES', nominal: 100, number: ' w1234567890' },
-        { banknote: 'Dollars', nominal: 100, number: ' e1234567890' },
-        { banknote: 'Dollars', nominal: 50, number: ' c1234567890' },
-        { banknote: 'RUBLES', nominal: 100, number: ' r1234567890' },
-        { banknote: 'Dollars', nominal: 50, number: ' x1234567890' },
-        { banknote: 'RUBLES', nominal: 50, number: ' v1234567890' },
+    let [message, setMessage] = useState([
+        {message: 'message1'},
+        {message: 'message2'},
+        {message: 'message3'},
     ])
 
-    const [filter, setFilter] = useState('all')
 
-
-    let currentMoney = money;
-    if (filter === 'ruble') {
-        currentMoney = money.filter((filteredMoney) => filteredMoney.banknote === 'RUBLES')
-    }
-    if (filter === 'dollar') {
-        currentMoney = money.filter((filteredMoney) => filteredMoney.banknote === 'Dollars')
-    }
-
-
-    const onClickFilterHandler = (nameButton: FilterType) => {
-        setFilter(nameButton);
-    }
 
     return (
-       <>
-           <NewComponentForFilterTask
-               currentMoney={currentMoney}
-               onClickFilterHandler={onClickFilterHandler}/>
-       </>
+       <div className={'App'}>
+           <div>
+               <input/>
+               <button>+</button>
+           </div>
+       </div>
     )
 }
 
